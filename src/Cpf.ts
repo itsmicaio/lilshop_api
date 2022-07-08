efa7164fd597d1d72cedd795527e71a63f79093e
Caio Fuzatto <caio.mendonca@vilaapps.com.br>
2022-07-08 15:36:46 -0300

    refactor: refatorando logica de calculo de digitos no cpf

--- src/Cpf.ts
+++ src/Cpf.ts
@@ -1,32 +1,29 @@
-import CpfValidator from "./CpfValidator";
-
 export default class Cpf {
-  validator: CpfValidator
+  value: string;
 
-  constructor(readonly documentNumber: string) {
-    this.validator = new CpfValidator(this.getDigitsArray())
-    if (!this.isValid(this.documentNumber)) throw new Error("Invalid CPF");
-  }
-
-  getDigitsArray() {
-    return this.getCleanDocumentNumber().split("")
-  }
-
-  getCleanDocumentNumber() {
-    return this.documentNumber.replace(/\D+/g, "");
+  constructor(cpf: string) {
+    if (!this.isValid(cpf)) throw new Error("Invalid CPF");
+    this.value = cpf
   }
 
   private isValid(cpf: string) {
     if (this.isBlank(cpf)) return false
-    cpf = this.getCleanDocumentNumber()
+    cpf = this.removeNonDigits(cpf)
     if (!this.isValidLength(cpf)) return false
     if (this.areEqualDigits(cpf)) return false
-
-    return this.validator.test();
+    const digit_1 = this.calculateDigit(cpf, 10)
+    const digit_2 = this.calculateDigit(cpf, 11)
+    const checkDigits = cpf.slice(9)  
+    const calculatedDigits = `${digit_1}${digit_2}`;
+    return checkDigits === calculatedDigits
   }
 
   private isBlank(cpf: string) {
     return !cpf
+  }
+
+  private removeNonDigits(cpf: string) {
+    return cpf.replace(/\D+/g, "");
   }
   
   private isValidLength(cpf: string) {
@@ -37,4 +34,15 @@ export default class Cpf {
     const [firstDigit] = cpf
     return [...cpf].every(digit => digit === firstDigit)
   }
+
+  private calculateDigit (cpf: string, multiplier: number) {
+		let total = 0;
+		for (const digit of cpf) {
+			if (multiplier > 1) {
+				total += parseInt(digit) * multiplier--;
+			}
+    };
+		const rest = total%11;
+		return (rest < 2) ? 0 : 11 - rest;
+	}
 }
