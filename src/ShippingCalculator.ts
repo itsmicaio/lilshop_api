19b9b95901c68ada9c9f3f29d84185f04477abab
Caio Fuzatto <caio.mendonca@vilaapps.com.br>
2022-07-10 22:21:57 -0300

    feat: adicionando calculo de vários produtos

--- src/ShippingCalculator.ts
+++ src/ShippingCalculator.ts
@@ -4,12 +4,20 @@ export default class ShippingCalculator {
   MIN_SHIPPING_VALUE = 10.0
 
   constructor(
-    readonly dimesion: Dimension,
+    readonly dimesions: Dimension[],
     readonly distance: number
   ) {}
 
   calculate() {
-    const value = this.distance * this.dimesion.calculateVolume() * (this.dimesion.calculateDensity() / 100)
+    const value = this.calculateAll()
     return value < this.MIN_SHIPPING_VALUE ? this.MIN_SHIPPING_VALUE : value
   };
+
+  private calculateAll() {
+    let acummulator = 0
+    for (const dimesion of this.dimesions) {
+      acummulator += this.distance * dimesion.calculateVolume() * (dimesion.calculateDensity() / 100)
+    }
+    return acummulator
+  }
 }
--- test/ShippingCalculator.test.ts
+++ test/ShippingCalculator.test.ts
@@ -2,15 +2,23 @@ import Dimension from "../src/Dimension";
 import ShippingCalculator from "../src/ShippingCalculator";
 
 test("Deve calcular o valor do frete com a taxa minima", function () {
-  const shippingCalculator = new ShippingCalculator(new Dimension(20, 15, 10, 1), 1000)
+  const shippingCalculator = new ShippingCalculator([new Dimension(20, 15, 10, 1)], 1000)
   expect(shippingCalculator.calculate()).toBe(10.0)
 })
 
 const dimensions = [
-  {shippingCalculator: new ShippingCalculator(new Dimension(100, 30, 10, 3), 1000), value: 30.0},
-  {shippingCalculator: new ShippingCalculator(new Dimension(200, 100, 50, 40), 1000), value: 400.0}
+  {shippingCalculator: new ShippingCalculator([new Dimension(100, 30, 10, 3)], 1000), value: 30.0},
+  {shippingCalculator: new ShippingCalculator([new Dimension(200, 100, 50, 40)], 1000), value: 400.0}
 ]
 
 test.each(dimensions)("Deve calcular o valor do frete", function ({shippingCalculator, value}) {
   expect(shippingCalculator.calculate()).toBe(value)
+})
+
+test("Deve calcular o frete para vários produtos", function () {
+  const shippingCalculator = new ShippingCalculator([
+    new Dimension(100, 30, 10, 3),
+    new Dimension(200, 100, 50, 40)
+  ], 1000)
+  expect(shippingCalculator.calculate()).toBe(430)
 })
