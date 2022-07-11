ee295c9dd3d0dc4a35a825db389e393bf9791695
Caio Fuzatto <caio.mendonca@vilaapps.com.br>
2022-07-10 23:15:15 -0300

    feat: adicionando sinon para testar data de cupom

--- test/Order.test.ts
+++ test/Order.test.ts
@@ -2,6 +2,7 @@ import Product from '../src/Product'
 import Order from '../src/Order'
 import Coupon from '../src/Coupon';
 import Dimension from '../src/Dimension';
+import Sinon from 'sinon';
 
 let product1: Product;
 let product2: Product;
@@ -39,9 +40,8 @@ beforeEach(function () {
 })
 
 test("Deve calcular o valor total de um pedido com 1 produto", function () {
-  order.addProduct(product1, 1)
-
-  expect(order.total()).toBe(299.99 + 400)
+  order.addProduct(product1, 1);
+  expect(order.total()).toBe(299.99 + 400);
 })
 
 test("Deve calcular o valor total de um pedido com 3 produtos", function () {
@@ -49,9 +49,8 @@ test("Deve calcular o valor total de um pedido com 3 produtos", function () {
     { product: product1, quantity: 1 },
     { product: product2, quantity: 1 },
     { product: product3, quantity: 1 }
-  ])
-
-  expect(order.total()).toBe(599.97 + (400 * 3))
+  ]);
+  expect(order.total()).toBe(599.97 + (400 * 3));
 })
 
 test("Deve criar calcular o valor total de 3x um produto no pedido", function () {
@@ -59,32 +58,31 @@ test("Deve criar calcular o valor total de 3x um produto no pedido", function ()
     { product: product1, quantity: 3 },
     { product: product2, quantity: 1 },
     { product: product3, quantity: 1 }
-  ])
-
-  expect(order.total()).toBe(1199.95 + (400 * 5))
+  ]);
+  expect(order.total()).toBe(1199.95 + (400 * 5));
 })
 
 test("Deve criar um pedido com CPF inválido", function () {
   const invalidCpf = "444.568.126-99"
-
   expect(() => new Order(invalidCpf)).toThrow(new Error("Invalid CPF"))
 })
 
 test("Deve calcular um pedido com desconto de 3%", function () {
-  order.addProduct(product1, 1)
-  const coupon = new Coupon("ABC-3", 3, new Date())
-  order.addCoupon(coupon)
-
-  expect(order.total()).toBe(290.99 + 400)
+  order.addProduct(product1, 1);
+  const coupon = new Coupon("ABC-3", 3, new Date());
+  order.addCoupon(coupon);
+  const clock = Sinon.useFakeTimers({ now: new Date("2022-01-01T00:00:00") });
+  expect(order.total()).toBe(290.99 + 400);
+  clock.restore();
 })
 
 test("Deve aplicar um cupom de desconto vencido", function() {
-  const coupon = new Coupon("ABC-3", 3, new Date("2022-01-01T00:00:00"))
-  expect(() => order.addCoupon(coupon)).toThrow("This coupon is expired")
+  const coupon = new Coupon("ABC-3", 3, new Date("2022-01-01T00:00:00"));
+  expect(() => order.addCoupon(coupon)).toThrow("This coupon is expired");
 })
 
 test("Deve criar um pedido com quantidade negativa", function () {
-  expect(() => order.addProduct(product1, -1)).toThrow("Invalid quantity")
+  expect(() => order.addProduct(product1, -1)).toThrow("Invalid quantity");
 })
 
 test("Deve adicionar um produto já adicionado ao pedido", function () {
@@ -92,6 +90,6 @@ test("Deve adicionar um produto já adicionado ao pedido", function () {
     { product: product1, quantity: 1 },
     { product: product2, quantity: 1 },
     { product: product3, quantity: 1 }
-  ])
-  expect(() => order.addProduct(product2, 1)).toThrow("Product is already added")
+  ]);
+  expect(() => order.addProduct(product2, 1)).toThrow("Product is already added");
 })
