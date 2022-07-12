8c32099c9f0f0f8a0ab2befa6b5d2ffaae89fbc1
Caio Fuzatto <caio.mendonca@vilaapps.com.br>
2022-07-12 16:54:18 -0300

    refactor: aplication ; e removendo importações erradas

--- src/ShippingCalculator.ts
+++ src/ShippingCalculator.ts
@@ -1,12 +1,11 @@
-import OrderProduct from "./OrderProduct";
 import Product from "./Product";
 
 export default class ShippingCalculator {
-  static MIN_SHIPPING_VALUE = 10.0
+  static MIN_SHIPPING_VALUE = 10.0;
 
   static calculate(product: Product, distance: number = 1000) {
-    const value = distance * product.getVolume() * (product.getDensity() / 100)
-    if (!value) return 0
-    return Math.max(value, ShippingCalculator.MIN_SHIPPING_VALUE)
+    const value = distance * product.getVolume() * (product.getDensity() / 100);
+    if (!value) return 0;
+    return Math.max(value, ShippingCalculator.MIN_SHIPPING_VALUE);
   };
 }
